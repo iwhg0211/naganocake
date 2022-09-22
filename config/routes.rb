3af6580e46da1538c95f46a1a17560dbb4b6d6d0
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-22 01:32:04 +0000

    [Add]items,top,about to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: "hemes#top"
+    resources :items
+    get 'homes/about'
+  end
+  namespace :public do
+    root to: "homes#top"
+    resources :items
+    get 'homes/about'
+  end
 end
