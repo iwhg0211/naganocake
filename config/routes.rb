ac4381b2cfee7007f39161d16a7747ed960ee26b
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-23 09:43:11 +0000

    [Add]admin,public routing to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,27 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :admin do
-    get 'create_status/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   namespace :admin do
     root to: "hemes#top"
     resources :items
+    resources :create_status, only: [:update]
+    resources :orders, only: [:show,:update]
+    resources :sessions, only: [:index,:show, :edit, :update]
+    resources :order_details, only: [:update]
   end
+  
   namespace :public do
     root to: "homes#top"
     resources :items
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :customera, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :registrations, only: [:new, :create]
     get 'homes/about'
   end
+  
 end
