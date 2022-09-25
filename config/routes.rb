b3a33c301b5455e4af7f77cc5df2806bd9026584
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-25 11:58:52 +0000

    [Add]devise_controller public,admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'homes/top'
-  devise_for :users
-  root to: "homes#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root to: "homes#top"
+  devise_for :customers
+  devise_for :admins
   
+  root to: "homes#top"
+  get 'homes/about'
+  resources :items
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :customera, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :registrations, only: [:new, :create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :admin do
     root to: "homes#top"
     resources :items
@@ -13,17 +21,8 @@ Rails.application.routes.draw do
     resources :orders, only: [:show,:update]
     resources :sessions, only: [:index,:show, :edit, :update]
     resources :order_details, only: [:update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
   end
-  
-  namespace :public do
-    resources :items
-    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
-    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-    resources :customera, only: [:show, :edit, :update, :unsubscribe, :withdraw]
-    resources :sessions, only: [:new, :create, :destroy]
-    resources :registrations, only: [:new, :create]
-    get 'homes/about'
-  end
-  
+
 end
