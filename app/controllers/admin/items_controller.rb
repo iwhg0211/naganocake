43bb7b19ee4dd2188099efc524be04ccb96fe062
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-29 02:57:24 +0000

    [Add]genre-index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,11 @@
 class Admin::ItemsController < ApplicationController
   
   def index
+    @items = Item.all
   end
   
   def show
+    @item = Item.find(params[:id])
   end
   
   def new
--- db/migrate/20220925113318_devise_create_customers.rb
+++ db/migrate/20220925113318_devise_create_customers.rb
@@ -4,6 +4,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_deleted
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
