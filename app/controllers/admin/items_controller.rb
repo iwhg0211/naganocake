fa016f0de48e9078ba9ab5850ba2a9bd2e208f82
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-09-29 03:37:29 +0000

    [Add]show of item's view to views/admin/items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,23 +1,33 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def new
+    @item = Item.new
   end
-  
+
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_image, :name, :introduction, :price, :genre_id, :is_active)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
   belongs_to :genre
+  
+  has_one_attached :item_image
 end
