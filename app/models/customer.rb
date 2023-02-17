67c3fbd99118fd07559ee6e07f441dcca5754c10
EC2 Default User <ec2-user@ip-172-31-44-123.ap-northeast-1.compute.internal>
2023-02-17 01:28:24 +0000

    [fix]customer_validate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,11 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :addresses
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+
   def active_for_authentication?
     super && (is_deleted != true)
   end
